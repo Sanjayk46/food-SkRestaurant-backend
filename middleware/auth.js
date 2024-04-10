132fe8388703e92071536cdcef1e09a811e25329
Sanjayk46 <138973406+Sanjayk46@users.noreply.github.com>
2024-04-10 18:08:30 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,27 +1,47 @@
- const { verify } = require('jsonwebtoken');
+const { verify } = require('jsonwebtoken');
 const UNAUTHORIZED = 401;
-
 const authMiddleware = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(UNAUTHORIZED).send({
-            message: "Invalid authorization header format"
-        });
-    }
+const authHeader = req.headers['authorization'];
+const token = authHeader.split(' ')[1];
+  console.log(token);
+  if (!token) return res.status(UNAUTHORIZED).send();
+   try {
+    const decoded = verify(token, process.env.JWT_SECRET); // Assuming jwt is imported
+    req.user = decoded;
 
-    const token = authHeader.split(' ')[1];
-
-    try {
-        const decoded = verify(token, process.env.JWT_SECRET); // Assuming jwt is imported
-        req.user = decoded;
-        next(); // Call next middleware
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send({
-            message: error.message
-        });
-    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      message:error.message
+  });
+  }
+  
 };
-
 module.exports = authMiddleware;
+// const { verify } = require('jsonwebtoken');
+// const UNAUTHORIZED = 401;
+
+// const authMiddleware = (req, res, next) => {
+//     const authHeader = req.headers['authorization'];
+    
+//     if (!authHeader || !authHeader.startsWith('Bearer ')) {
+//         return res.status(UNAUTHORIZED).send({
+//             message: "Invalid authorization header format"
+//         });
+//     }
+
+//     const token = authHeader.split(' ')[1];
+
+//     try {
+//         const decoded = verify(token, process.env.JWT_SECRET); // Assuming jwt is imported
+//         req.user = decoded;
+//         next(); // Call next middleware
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(500).send({
+//             message: error.message
+//         });
+//     }
+// };
+
+// module.exports = authMiddleware;
